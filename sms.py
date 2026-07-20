6e297fa374996603333e1068fb30f0f079ac9d04
Jay Raj <haldarjayraj@gmail.com>
2026-07-20 16:48:08 +0530

    updates

--- sms.py
+++ sms.py
@@ -1,6 +1,14 @@
 '''Student Management System using FastAPI'''
 from fastapi import FastAPI, Path, HTTPException, Query
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field, computed_field
+from typing import Annotated, Literal, Optional
+import json
+
+app = FastAPI()
+
+class Student(BaseModel):
+    
 
 print ("making github greeen")
 
