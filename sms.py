227a47082d389842ce5c8667dfe39d529e37efee
Jay Raj <haldarjayraj@gmail.com>
2026-07-20 16:42:56 +0530

    updates

--- sms.py
+++ sms.py
@@ -1,9 +1,9 @@
 '''Student Management System using FastAPI'''
 from fastapi import FastAPI, Path, HTTPException, Query
+from fastapi.responses import JSONResponse
+
+print ("making github greeen")
 
 
 
-
-
-print ("making github green")
 
