3b35a804a4a8d9d6ccdc5406c379b5e60f54ede9
Jay Raj <haldarjayraj@gmail.com>
2026-07-22 19:26:11 +0530

    added the add student feature

--- sms.py
+++ sms.py
@@ -7,9 +7,9 @@ import json
 
 app = FastAPI()
 
-class Student(BaseModel):
+class Student(BaseModel): #This is the Student pydantic model
 
-    enroll : Annotated[str, Field(..., description='ID of the student', examples='LNCFBTC00001')]
+    enroll : Annotated[str, Field(..., description='Enrollment no. of the student', examples='LNCFBTC00001')]
     roll_no : Annotated[str, Field(..., description='Roll no. of the student', examples=['001'])]
     first_name : Annotated[str, Field(..., description='First name of the student')]
     last_name : Annotated[str, Field(..., description=':Last name of the student')]
@@ -24,7 +24,7 @@ class Student(BaseModel):
 
 #-----------------Updating student details--------------------
 
-class StudentUpdate(BaseModel):
+class StudentUpdate(BaseModel): #this is the StudentUpdate pydantic model
     roll_no : Annotated[Optional[str], Field(default=None)]
     first_name : Annotated[Optional[str], Field(default=None)]
     last_name : Annotated[Optional[str], Field(default=None)]
@@ -34,3 +34,41 @@ class StudentUpdate(BaseModel):
     mother_name : Annotated[Optional[str], Field(default=None)]
     course : Annotated[Optional[str], Field(default=None)]
     city : Annotated[Optional[str], Field(default=None)]
+    #as we have use none datatype it automatically takes any input given to it
+    #Annotated is used to add description
+    #Literal is used to give options
+
+#------------Loading data-----------
+#whenever we call this function it will load the data 
+def load_data():
+    with open('students.json', 'r') as f:#opening it in read mode
+        data = json.load(f)
+    return data
+
+#----------Saving data-------------
+
+def save_data(data):
+    with open('students.json', 'w') as f:
+        json.dump(data, f)
+#jo data hame input mil raha hai ham use data me dump kar rahe hai
+
+
+#--------------------Endpoints---------------------
+
+@app.get('/')
+def hello():
+    return("STUDENT MANAGEMENT SYSTEM") #This is the first end point of our api
+
+@app.post('/add')
+def add_stu(student: Student):
+    data = load_data()
+    #check krna hai ki student already exist ya nahi
+    if student.enroll in data:
+        raise HTTPException(status_code=400, detail='Student already exist')
+
+    data[student.enroll] = student.model_dump(exclude=["enroll"]) 
+
+    save_data(data)
+
+    return JSONResponse(status_code=201, content={'message': 'Student created successfully'})
+
